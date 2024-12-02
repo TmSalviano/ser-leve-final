6478aba5540b1de7eff022c649e56b177503a050
TmSalviano <tiagomsalviano@outlook.com>
2024-12-02 16:29:16 -0300

    Maybe we are done now?

--- FrontA3/ser-leve/src/components/Main.jsx
+++ FrontA3/ser-leve/src/components/Main.jsx
@@ -115,7 +115,7 @@ function Main() {
   }, []);
 
   return (
-    <main className="lg:w-3/4 rounded-lg space-y-6 flex flex-col flex-grow mt-96">
+    <main className="lg:w-3/4 rounded-lg space-y-6 flex flex-col flex-grow h-full">
       {/* Create new receita */}
       <NewPost />
 
